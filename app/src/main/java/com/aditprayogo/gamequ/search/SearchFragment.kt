b7f3ca4286d8e47d7173ba9922f88e32da9ae3df
Aditiya Prayogo <adit.ihzar@gmail.com>
2024-05-23 23:32:08 +0700

    Adding presentation layer and fix some kotlin, room and dagger hilt version

---
+++ app/src/main/java/com/aditprayogo/gamequ/search/SearchFragment.kt
@@ -0,0 +1,104 @@
+package com.aditprayogo.gamequ.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aditprayogo.core.domain.entity.GameData
+import com.aditprayogo.core.utils.LoaderState
+import com.aditprayogo.core.utils.setGone
+import com.aditprayogo.core.utils.setVisible
+import com.aditprayogo.gamequ.databinding.FragmentSearchBinding
+import com.aditprayogo.gamequ.game.GameAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment : Fragment() {
+
+    val binding: FragmentSearchBinding by lazy {
+        FragmentSearchBinding.inflate(layoutInflater)
+    }
+
+    private val gameAdapter: GameAdapter by lazy { GameAdapter() }
+
+    private val searchViewModel by viewModels<SearchViewModel>()
+
+    private val gameDataEntity = mutableListOf<GameData>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+        initObservers()
+        searchGame()
+    }
+
+    private fun searchGame() {
+        with(binding) {
+            svSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+                androidx.appcompat.widget.SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    query?.let {
+                        if (it.isNotEmpty()) {
+                            gameDataEntity.clear()
+                            searchViewModel.searchGames(it)
+                            svSearch.clearFocus()
+                        }
+                    }
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean = false
+            })
+        }
+    }
+
+    private fun initRecyclerView() {
+        binding.apply {
+            with(rvGame) {
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                adapter = gameAdapter
+            }
+        }
+    }
+
+    private fun initObservers() {
+        with(searchViewModel) {
+            state.observe(viewLifecycleOwner) {
+                handleState(it)
+            }
+            resultGameApi.observe(viewLifecycleOwner) {
+                handleResultGameApi(it)
+            }
+        }
+    }
+
+    private fun handleResultGameApi(it: List<GameData>) {
+        gameAdapter.submitList(it)
+    }
+
+    private fun handleState(it: LoaderState) {
+        with(binding) {
+            if (it is LoaderState.ShowLoading) {
+                baseLoading.root.setVisible()
+                rvGame.setGone()
+            } else {
+                baseLoading.root.setGone()
+                rvGame.setVisible()
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/aditprayogo/gamequ/search/SearchViewModel.kt
@@ -0,0 +1,60 @@
+package com.aditprayogo.gamequ.search
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aditprayogo.core.domain.entity.GameData
+import com.aditprayogo.core.domain.usecases.GameUseCase
+import com.aditprayogo.core.utils.LoaderState
+import com.aditprayogo.core.utils.ResultState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Created by Aditiya Prayogo.
+ */
+interface SearchViewModelContract {
+    fun searchGames(search: String)
+}
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val gameUseCase: GameUseCase
+) : ViewModel(), SearchViewModelContract {
+
+    private val _state = MutableLiveData<LoaderState>()
+    val state: LiveData<LoaderState> get() = _state
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
+
+    private val _networkError = MutableLiveData<Boolean>()
+    val networkError: LiveData<Boolean> get() = _networkError
+
+    private val _resultGameApi = MutableLiveData<List<GameData>>()
+    val resultGameApi: LiveData<List<GameData>> get() = _resultGameApi
+
+    override fun searchGames(search: String) {
+        _state.value = LoaderState.ShowLoading
+        viewModelScope.launch {
+            gameUseCase.searchGames(search).collect {
+                when (it) {
+                    is ResultState.Success -> {
+                        _resultGameApi.postValue(it.data!!)
+                        _state.value = LoaderState.HideLoading
+                    }
+                    is ResultState.Error -> {
+                        _error.postValue(it.error)
+                    }
+                    is ResultState.NetworkError -> {
+                        _networkError.postValue(true)
+                    }
+                }
+            }
+        }
+    }
+
+}
